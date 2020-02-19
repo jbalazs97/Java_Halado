e9600cc147acbf870f0fece53eb6cbd011168b78
Balazs <juhaszbalazs855@gmail.com>
2020-02-19 15:42:46 +0100

    Vegleges

--- 01/bolygok/src/main/java/hu/me/krz/haladojava/Astronomer.java
+++ 01/bolygok/src/main/java/hu/me/krz/haladojava/Astronomer.java
@@ -13,5 +13,20 @@ public class Astronomer {
 
 	public List<Planet> getDiscoveredPlanets() {
 	return DiscoveredPlanets;
+	
+	}
+	
+	
+	public void observeTheSky() {
+			for (int i = 0; i < 10; i++) {
+					String nev = this.name + (i + 1);
+					Point pozicio = new Point(i * 10, 0, 0);
+					double atmero = new Random().nextInt(1000000);
+
+				
+	DiscoveredPlanets.add(new Planet(atmero, nev, pozicio));
+		
+	}
 	}
 }
+	
